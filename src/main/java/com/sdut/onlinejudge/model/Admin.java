51542103aa287ef7754307003699caae8af86c7a
QXQZX <501966782@qq.com>
2019-12-23 22:07:41 +0800

    add PageHelper && statController etc.

---
+++ src/main/java/com/sdut/onlinejudge/model/Admin.java
@@ -0,0 +1,16 @@
+package com.sdut.onlinejudge.model;
+
+import lombok.Data;
+
+/**
+ * @Author: Devhui
+ * @Date: 2019/12/23 18:31
+ * @Email: devhui@ihui.ink
+ * @Version: 1.0
+ */
+
+@Data
+public class Admin {
+    private String username;
+    private String password;
+}
